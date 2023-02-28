861b76f8f06b7a1405720a3d4e00480255d854d7
Rakesh Roshan <roshanrakesh7362@gmail.com>
2023-03-01 03:58:13 +0530

    Main Files

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=/?simply_static_page=34">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/?simply_static_page=34";
+		</script>
+
+		<p>You are being redirected to <a href="/?simply_static_page=34">/?simply_static_page=34</a></p>
+	</body>
+</html>
